15afc2194d53e0a0b9d6bc825a8df567abef69d1
Pasdinspy <140913724+Pasdinspy@users.noreply.github.com>
2024-04-09 11:26:25 +0200

    Update gestionfraisforfait.php

--- etudiant2/gestionfraisforfait.php
+++ etudiant2/gestionfraisforfait.php
@@ -3,67 +3,6 @@
 <head>
 	<title>Gestion des Frais</title>
 	<link rel="stylesheet" href="index.css">
-	<style>
-
-		h1 {
-			font-size: 36px;
-			font-style: italic;
-			display: inline-block;
-			margin: 0;
-			padding: 10px;
-		}
-		img {
-			display: inline-block;
-			height: 50px;
-			width: 50px;
-			margin-left: 10px;
-		}
-		form {
-			margin-top: 50px;
-			margin-bottom: 50px;
-		}
-		label {
-			display: block;
-			margin-bottom: 10px;
-		}
-		input[type="text"], input[type="number"] {
-			display: block;
-			margin-bottom: 20px;
-			padding: 5px;
-			border-radius: 5px;
-			border: none;
-			box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-			width: 200px;
-		}
-		input[type="submit"] {
-			background-color: #333;
-			color: white;
-			padding: 10px 20px;
-			border: none;
-			border-radius: 5px;
-			box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-			cursor: pointer;
-		}
-		table {
-			margin-top: 50px;
-			border-collapse: collapse;
-			width: 100%;
-		}
-		th, td {
-			padding: 10px;
-			text-align: left;
-			border-bottom: 0px solid white;
-		}
-		th {
-			color: white;
-		}
-		.maDiv {
-			width: 100%;
-			height: 50%;
-			background-color: white;
-			color: rgb(121, 153, 221);
-		}
-	</style>
 </head>
 <body class="gestrionfraisforfait-body">
 	<?php
@@ -72,44 +11,44 @@
     }
 	?>
 
-	<div class="maDiv">
-		<h1><a href="http://lab.sio-estran.fr:18102/KERFOURN/depot/fichedefrais.php?VIS_ID=<?= $VIS_ID ?>">Gestion des Frais </a><i class="fa-solid fa-hippo"></i></h1>
+	<div class="gestionfraisforfait-Div">
+		<h1 class="gestrionfraisforfait-h1"><a href="http://lab.sio-estran.fr:18102/KERFOURN/depot/fichedefrais.php?VIS_ID=<?= $VIS_ID ?>">Gestion des Frais </a><i class="fa-solid fa-hippo"></i></h1>
 	</div>
 	
-	<form action="insert.php" method="get">
+	<form action="insert.php" method="get" class="gestrionfraisforfait-form">
 		<input type="hidden" name="VIS_ID" value="<?php echo $VIS_ID; ?>">
 		<fieldset>
 		
 			<legend>Saisie</legend>
-			<label for="month">Mois (2 chiffres)</label>
-			<input type="text" id="month" name="month" required>
-			<label for="year">Année (4 chiffres)</label>
-			<input type="text" id="year" name="year" required>
+			<label for="month" class="gestionfraisforfait-label">Mois (2 chiffres)</label>
+			<input type="text" id="month" name="month" required class="gestionfraisforfait-input">
+			<label for="year" class="gestionfraisforfait-label">Année (4 chiffres)</label>
+			<input type="text" id="year" name="year" required class="gestionfraisforfait-input">
 		</fieldset>
 		<fieldset>
 		
 			<legend>Frais au forfait</legend>
-			<table>
+			<table class="gestionfraisforfait-table">
 				<tr>
-					<th>Repas midi :</th>
-					<td><input type="number" id="repas" name="repas" required></td>
+					<th class="gestionfraisforfait-th">Repas midi :</th>
+					<td class="gestionfraisforfait-td"><input type="number" id="repas" name="repas" required class="gestionfraisforfait-input"></td>
 					
 				</tr>
 				<tr>
-					<th>Nuitées :</th>
-					<td><input type="number" id="nuitees" name="nuitees" required></td>
+					<th class="gestionfraisforfait-th">Nuitées :</th>
+					<td class="gestionfraisforfait-td"><input type="number" id="nuitees" name="nuitees" required class="gestionfraisforfait-input"></td>
 				</tr>
 				<tr>
-					<th>Étape :</th>
-					<td><input type="number" id="etape" name="etape" required></td>
+					<th class="gestionfraisforfait-th">Étape :</th>
+					<td class="gestionfraisforfait-td"><input type="number" id="etape" name="etape" required class="gestionfraisforfait-input"></td>
 				</tr>
 				<tr>
-					<th>Km :</th>
-					<td><input type="number" id="km" name="km" required></td>
+					<th class="gestionfraisforfait-th">Km :</th>
+					<td class="gestionfraisforfait-td"><input type="number" id="km" name="km" required class="gestionfraisforfait-input"></td>
 				</tr>
 			</table>
 		</fieldset>
-		<input type="submit" value="Soumettre la requête">
+		<input type="submit" value="Soumettre la requête" class="gestionfraisforfait-input">
 	</form>
 </body>
 </html>
